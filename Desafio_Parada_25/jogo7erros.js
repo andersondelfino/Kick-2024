52461a7112bb42626e363b8b981c694d9a03b73c
andersondelfino <andersonaugusto808@gmail.com>
2024-06-10 10:42:16 -0300

    Atualizações

--- Desafio_Parada_25/jogo7erros.js
+++ Desafio_Parada_25/jogo7erros.js
@@ -1,3 +1,4 @@
+//2° (), 3° "Ben 10
 let desenhosAnimados = [
     "Bob Esponja",
     "Os Padrinhos Mágicos",
@@ -14,6 +15,7 @@ let desenhosAnimados = [
 let indiceSubstituicao = 0; // Índice para controlar a substituição
 
 // Função para exibir a lista de desenhos animados
+//4° getElegementClass
 function exibirLista() {
     const lista = document.getElementById('lista-desenhos'); 
     lista.innerHTML = ''; // Limpa a lista antes de preenchê-la novamente
@@ -25,11 +27,12 @@ function exibirLista() {
     });
 }
 
+// 5° adição da cosnt inputElemento
 function adicionarDesenho() {
     const inputElemento = document.getElementById('novo-desenho');
     const novoDesenho = inputElemento.value.trim(); 
     
-    // 5. Verificação da condição com valor do input corrigido
+    // 5° novoDesenho.trim, 6° exibirLista;
     if (novoDesenho !== '') {
         desenhosAnimados[indiceSubstituicao] = novoDesenho; // Substitui o elemento no índice atual
         indiceSubstituicao = (indiceSubstituicao + 1) % desenhosAnimados.length; // Atualiza o índice para a próxima substituição
@@ -39,4 +42,5 @@ function adicionarDesenho() {
 }
 
 // Exibe a lista inicial quando a página carrega
+//7°window.onload
 exibirLista(); 
